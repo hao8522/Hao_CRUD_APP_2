3df7559f229c993bf41c3c5d283040bfc07a9e9f
Hao <wanghao8522@gmail.com>
2017-12-15 10:26:16 +1300

    product module
    - add product with ajax
    -modify product with ajax
    - delete proeuct with ajax
    - add product with vaildation
    -modify product with validation

--- Hao_CRUD_APP_2/Controllers/ProductController.cs
+++ Hao_CRUD_APP_2/Controllers/ProductController.cs
@@ -20,5 +20,58 @@ namespace Hao_CRUD_APP_2.Controllers
 
             return View("Products");
         }
+
+        [HttpPost]
+        public ActionResult AddProduct(Product addProduct)
+        {
+            int result = new ProductService().AddProduct(addProduct);
+
+            if(result > 0)
+            {
+
+                return Content("success");
+            }
+            else
+            {
+                return Content("error");
+            }
+        }
+
+        public ActionResult DeleteProduct(int productId)
+        {
+            int result = new ProductService().DeleteProduct(productId);
+
+            if (result > 0)
+            {
+                return Content("deleteProduct");
+            }
+            else
+            {
+                return Content("delError");
+            }
+        }
+
+        public ActionResult LoadingModifyProduct(int productId)
+        {
+            Product product = new ProductService().GetProductById(productId);
+
+            return View("ModifyProduct", product);
+        }
+
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult ModifyProduct(Product product)
+        {
+            int result = new ProductService().ModidyProduct(product);
+
+            if (result > 0)
+            {
+                return Content("success");
+            }
+            else
+            {
+                return Content("error");
+            }
+        }
     }
 }
