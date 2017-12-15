4ffa9f7480615b1668648e4e3793a1285bc7d3ca
Hao <wanghao8522@gmail.com>
2017-12-15 13:34:00 +1300

    customer module
    - add customer with ajax
    -modify customer with ajax
    -delete customer with ajax
    - add customer with ajax validation
    -modify customer with ajax validation

--- Hao_CRUD_APP_2/Controllers/CustomerController.cs
+++ Hao_CRUD_APP_2/Controllers/CustomerController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Hao_CRUD_APP_2.Models;
+using Hao_CRUD_APP_2.Service;
 
 namespace Hao_CRUD_APP_2.Controllers
 {
@@ -11,7 +13,63 @@ namespace Hao_CRUD_APP_2.Controllers
         // GET: Customer
         public ActionResult CustomersList()
         {
+
+            List<Customer> customerList = new CustomerService().GetAllCustomer();
+
+            ViewBag.list = customerList;
+
             return View("Customers");
+        }
+
+
+        public ActionResult LoadingModifyCustomer(int productId)
+        {
+            Customer customer = new CustomerService().GetCustomerById(productId);
+
+            return View("ModifyCustomer", customer);
+        }
+
+        [HttpPost]
+        public ActionResult ModifyCustomer(Customer customer)
+        {
+            int result = new CustomerService().ModifyCustomer(customer);
+
+            if (result > 0)
+            {
+                return Content("success");
+            }
+            else
+            {
+                return Content("error");
+            }
+        }
+
+        public ActionResult DeleteCustomer(int customerId)
+        {
+            int result = new CustomerService().DeleteCustomer(customerId);
+
+            if (result > 0)
+            {
+                return Content("success");
+            }
+            else
+            {
+                return Content("error");
+            }
+        }
+        [HttpPost]
+        public ActionResult AddCustomer(Customer customer)
+        {
+            int result = new CustomerService().AddCustomer(customer);
+
+            if (result > 0)
+            {
+                return Content("success");
+            }
+            else
+            {
+                return Content("error");
+            }
         }
     }
 }
--- Hao_CRUD_APP_2/Service/CustomerService.cs
+++ Hao_CRUD_APP_2/Service/CustomerService.cs
@@ -3,11 +3,63 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Data.Entity;
+using Hao_CRUD_APP_2.Models;
 
 namespace Hao_CRUD_APP_2.Service
 {
     public class CustomerService
     {
+
+        public int AddCustomer(Customer customer)
+        {
+            using (HAO_Entities db = new HAO_Entities())
+            {
+                db.Customers.Add(customer);
+
+                return db.SaveChanges();
+            }
+        }
+        public int ModifyCustomer(Customer customer)
+        {
+            using (HAO_Entities db = new HAO_Entities())
+            {
+                db.Customers.Attach(customer);
+                db.Entry<Customer>(customer).State = EntityState.Modified;
+                return db.SaveChanges();
+            }
+        }
+
+        public int DeleteCustomer(int customerId)
+        {
+            using (HAO_Entities db = new HAO_Entities())
+            {
+                Customer c = new Customer()
+                {
+                    Id = customerId
+                };
+
+                db.Customers.Attach(c);
+                db.Customers.Remove(c);
+                return db.SaveChanges();
+            }
+        }
+
+        public List<Customer> GetAllCustomer()
+        {
+            using(HAO_Entities db = new HAO_Entities())
+            {
+                return (from c in db.Customers select c).ToList();
+            }
+           
+        }
+
+        public Customer GetCustomerById(int productId)
+        {
+            using(HAO_Entities db = new HAO_Entities())
+            {
+                return ( from c in db.Customers where c.Id == productId select c ).FirstOrDefault();
+            }
+        }
     }
 }
